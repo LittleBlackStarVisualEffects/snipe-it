fbce94f591b72da645525c5688a5301bf4e82ead
snipe <snipe@snipe.net>
2026-03-10 15:11:45 +0000

    Set policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,10 +2,17 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+
 class UserPolicy extends SnipePermissionsPolicy
 {
     protected function columnName()
     {
         return 'users';
+    }
+    
+    public function manageContactInfo(User $user)
+    {
+        return $user->hasAccess('users.contact');
     }
 }
